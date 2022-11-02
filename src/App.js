8c6604a9e935238ec50f42bb2419db7ab4a9f05b
Baekshook <ecgo0106@naver.com>
2022-11-02 15:40:08 +0900

    푸터 퍼블리싱

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import GlobalStyle from "./GlobalStyle";
-import styled from "styled-components";
 import Header from "@components/templates/Header";
 import TopBanner from "@components/templates/TopBanner";
 import Events from "@components/templates/Events";
 import ItemsOnSale from "@components/templates/ItemsOnSale";
 import OpenseaTopCollections from "@components/templates/OpenseaTopCollections";
 import OnBoarding from "@components/templates/OnBoarding";
-
-const Footer = styled.footer`
-  width: 100%;
-  height: 240px;
-  background-color: floralwhite;
-  margin-top: 100px;
-`;
+import Footer from "@components/templates/Footer";
 
 
 function App() {
